888a3f0cab314e194e9b0111a0a86c7384b3eb2c
김승목 <tmdahr1245@naver.com>
2024-11-25 08:49:05 +0000

    static html

--- next.config.js
+++ next.config.js
@@ -5,24 +5,8 @@ const { homepage } = require('./package.json');
 const { NODE_ENV } = process.env;
 
 module.exports = withImages({
-  assetPrefix: (() => {
-    if (NODE_ENV === 'production' && homepage) {
-      try {
-        console.log('> Detected homepage url in package.json');
-        const { pathname } = new URL(homepage);
-        if (pathname !== '/') {
-          console.log(`> Apply \'${pathname}\' to assetPrefix(subPath)`);
-          return pathname;
-        }
-        return '';
-      } catch {
-        console.log('> Can not parse homepage URL not apply assetPrefix(subPath)');
-        return '';
-      }
-    }
-    return '';
-  })(),
-}, '.');
+  assetPrefix: '.'
+});
 // withCSS({
 // webpack: config => {
 //   config.resolve.alias['@'] = __dirname;
