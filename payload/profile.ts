2a764226a7121764c6338d1b6641694fb29ba386
김승목 <tmdahr1245@naver.com>
2024-11-25 10:35:16 +0000

    update index.html

--- payload/profile.ts
+++ payload/profile.ts
@@ -1,5 +1,5 @@
 import { faEnvelope, faPhone, faBlog } from '@fortawesome/free-solid-svg-icons';
-import { faFacebook, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
 
 import { faBell } from '@fortawesome/free-regular-svg-icons';
 import { IProfile } from '../component/profile/IProfile';
