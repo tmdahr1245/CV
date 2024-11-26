5df37a02a7c8e65fa56625e80a0b852a7cfbb356
김승목 <tmdahr1245@naver.com>
2024-11-26 04:59:25 +0000

    update index.html

--- payload/etc.ts
+++ payload/etc.ts
@@ -5,16 +5,23 @@ const etc: IEtc.Payload = {
 
   list: [
     {
-      title: 'SK Telecom',
-      subTitle: 'Intern',
+      title: 'SK Telecom (Intern)',
+      subTitle: 'Software Composition Analysis',
       startedAt: '2021-06',
       endedAt: '2021-08',
     },
     {
-      title: 'Incross',
-      subTitle: 'Backend Software Engineer',
+      title: 'Incross (Intern)',
+      subTitle: 'Backend & DataBase Procedure',
       startedAt: '2020-01',
       endedAt: '2020-06',
+    },
+    {
+      title: 'Software Maestro (10th)',
+      subTitle:
+        'External Activity Organized by ‘Ministry of Science and ICT’ and FKII(The Federation of Korean Information Industries)',
+      startedAt: '2019-04',
+      endedAt: '2019-11',
     },
   ],
 };
