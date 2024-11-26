88532f45a48eb16198fdb6f1c598c8b8ee9c4e2b
김승목 <tmdahr1245@naver.com>
2024-11-26 03:04:25 +0000

    update index.html

--- component/etc/index.tsx
+++ component/etc/index.tsx
@@ -22,7 +22,7 @@ export const Etc = {
 
 function Component({ payload }: PropsWithChildren<{ payload: Payload }>) {
   return (
-    <CommonSection title="ETC">
+    <CommonSection title="External Activities">
       <EducationRow payload={payload} />
     </CommonSection>
   );
