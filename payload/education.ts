1500313ef3254a1e12cc1c9fe38df9f83a34ef6f
김승목 <tmdahr1245@naver.com>
2024-11-26 02:56:10 +0000

    update index.html

--- payload/education.ts
+++ payload/education.ts
@@ -7,17 +7,17 @@ const education: IEducation.Payload = {
     {
       title: 'Korea University',
       subTitle: 'M.S. in Computer Science and Engineering',
-      descriptions: [
-        'Computer & Communication Security Laboratory (CCS Lab)',
-        'Thesis: AutoDBG: Deterministic Replay and Debugging for Robot Operating System',
-      ],
+      // descriptions: [
+      //   'Computer & Communication Security Laboratory (CCS Lab)',
+      //   'Thesis: AutoDBG: Deterministic Replay and Debugging for Robot Operating System',
+      // ],
       startedAt: '2020-09',
       endedAt: '2022-08',
     },
     {
       title: 'The Catholic University of Korea',
       subTitle: 'B.S. in Computer Science and Information Engineering',
-      descriptions: ['On leave for 2 years: mandatory military service at RoK ARMY'],
+      // descriptions: ['On leave for 2 years: mandatory military service at RoK ARMY'],
       startedAt: '2014-03',
       endedAt: '2020-08',
     },
