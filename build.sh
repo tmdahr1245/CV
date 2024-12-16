09d0074f7b79dc8d561a63347cb6d57dfa45b07b
tmdahr1245 <tmdahr1245@naver.com>
2024-12-16 14:11:19 +0900

    build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 python3 /workspace/cv/modLastUpdate.py
 npm run export
 python3 /workspace/cv/modPath.py
+python3 /workspace/cv/appendGATag.py
 git add .
 git commit -m "update index.html"
 git push
