028262ef62a8829dc84b8f175085cbf2af6d2900
김승목 <tmdahr1245@naver.com>
2024-12-20 02:39:22 +0000

    update index.html

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ python3 /workspace/cv/modLastUpdate.py
 npm run export
 python3 /workspace/cv/modPath.py
 python3 /workspace/cv/appendGATag.py
+cp /workspace/cv/*.pdf /workspace/cv/docs
 git add .
 git commit -m "update index.html"
 git push
