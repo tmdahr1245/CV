91d2b6808408309d81b436bcf3a54f182db0431f
김승목 <tmdahr1245@naver.com>
2025-01-03 08:00:11 +0000

    update index.html

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 python3 /workspace/cv/modLastUpdate.py
+mv /workspace/cv/docs/kr /workspace/cv/backup
 npm run export
 python3 /workspace/cv/modPath.py
 python3 /workspace/cv/appendGATag.py
 cp /workspace/cv/*.pdf /workspace/cv/docs
 cp /workspace/cv/_config.yml /workspace/cv/docs
+mv /workspace/cv/backup/kr /workspace/cv/docs
 git add .
 git commit -m "update index.html"
 git push
--- build_kr.sh
+++ build_kr.sh
@@ -3,7 +3,8 @@
 python3 /workspace/cv/modLastUpdate.py
 mv payload payload_en
 mv payload_kr payload
-mv /workspace/cv/docs/index.html /workspace/cv/docs/index_en.html
+#mv /workspace/cv/docs/index.html /workspace/cv/docs/index_en.html
+mv /workspace/cv/docs /workspace/cv/backup
 # index.html 뿐만 아니라 모든 내용 백업
 npm run export
 python3 /workspace/cv/modPath.py
@@ -11,8 +12,13 @@ python3 /workspace/cv/appendGATag.py
 cp /workspace/cv/*.pdf /workspace/cv/docs
 mv payload payload_kr
 mv payload_en payload
-mv /workspace/cv/docs/index.html /workspace/cv/docs/index_kr.html
-mv /workspace/cv/docs/index_en.html /workspace/cv/docs/index.html
-git add .
-git commit -m "update index_kr.html"
+# mv /workspace/cv/docs/index.html /workspace/cv/docs/index_kr.html
+# mv /workspace/cv/docs/index_en.html /workspace/cv/docs/index.html
+mkdir /workspace/cv/docs/kr
+mv /workspace/cv/docs/*.html /workspace/cv/docs/kr
+mv /workspace/cv/docs/*.pdf /workspace/cv/docs/kr
+mv /workspace/cv/docs/_next /workspace/cv/docs/kr
+mv /workspace/cv/backup/* /workspace/cv/docs
+git add /workspace/cv/docs/kr
+git commit -m "update index.html kr"
 git push
