f372e3be9a0a6f54be11bff71f073282b20ef8f6
김승목 <tmdahr1245@naver.com>
2025-01-03 12:05:15 +0000

    update index.html

--- build_kr.sh
+++ build_kr.sh
@@ -3,19 +3,18 @@
 python3 /workspace/cv/modLastUpdate.py
 mv payload payload_en
 mv payload_kr payload
-#mv /workspace/cv/docs/index.html /workspace/cv/docs/index_en.html
+
 rm -rf /workspace/cv/backup
 mkdir /workspace/cv/backup
-mv /workspace/cv/docs /workspace/cv/backup
-# index.html 뿐만 아니라 모든 내용 백업
+mv /workspace/cv/docs/* /workspace/cv/backup
+
 npm run export
 python3 /workspace/cv/modPath.py
 python3 /workspace/cv/appendGATag.py
 cp /workspace/cv/*.pdf /workspace/cv/docs
 mv payload payload_kr
 mv payload_en payload
-# mv /workspace/cv/docs/index.html /workspace/cv/docs/index_kr.html
-# mv /workspace/cv/docs/index_en.html /workspace/cv/docs/index.html
+
 mkdir /workspace/cv/docs/KR
 mv /workspace/cv/docs/*.html /workspace/cv/docs/KR
 mv /workspace/cv/docs/*.pdf /workspace/cv/docs/KR
