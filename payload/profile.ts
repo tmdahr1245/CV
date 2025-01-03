3363fb4c4a123d891d77b1208dec2a8af510fb04
김승목 <tmdahr1245@naver.com>
2025-01-03 10:45:24 +0000

    update index.html

--- payload/profile.ts
+++ payload/profile.ts
@@ -34,13 +34,15 @@ const profile: IProfile.Payload = {
       icon: faLinkedin,
     },
     {
+      title: '한국어 버젼을 원한다면 여기를 클릭하세요.',
       link: 'https://tmdahr1245.github.io/CV/KR',
       icon: faCalendar,
     },
   ],
   notice: {
-    title: '.',
+    title: '한국어 버젼을 원한다면 여기를 클릭하세요.',
     icon: faBell,
+    link: 'https://tmdahr1245.github.io/CV/KR',
   },
 };
 
