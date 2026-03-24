8faa34d4d6d630451e8392531cb5000744276f8a
tmdahr1245 <tmdahr1245@users.noreply.github.com>
2026-03-24 20:27:27 +0900

    Update skill.ts

--- payload_kr/skill.ts
+++ payload_kr/skill.ts
@@ -26,35 +26,35 @@ const programmingLanguages: ISkill.Skill = {
   ],
 };
 
-// const kernelDevelopment: ISkill.Skill = {
-//   category: 'Robotics Skills',
-//   items: [
-//     {
-//       title: 'ROS2',
-//       // level: 3,
-//     },
-//     {
-//       title: 'Gazebo',
-//       // level: 3,
-//     },
-//     {
-//       title: 'Path Planning',
-//       // level: 2,
-//     },
-//     {
-//       title: 'Collision Avoidance',
-//       // level: 2,
-//     },
-//     {
-//       title: 'Exploration',
-//       // level: 2,
-//     },
-//     {
-//       title: 'DDS',
-//       // level: 2,
-//     },
-//   ],
-// };
+const kernelDevelopment: ISkill.Skill = {
+  category: 'Robotics Skills',
+  items: [
+    {
+      title: 'ROS2',
+      // level: 3,
+    },
+    {
+      title: 'Gazebo',
+      // level: 3,
+    },
+    {
+      title: 'Path Planning',
+      // level: 2,
+    },
+    {
+      title: 'Collision Avoidance',
+      // level: 2,
+    },
+    {
+      title: 'Exploration',
+      // level: 2,
+    },
+    {
+      title: 'DDS',
+      // level: 2,
+    },
+  ],
+};
 
 const virtualization: ISkill.Skill = {
   category: 'SW Analysis',
